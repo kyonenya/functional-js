05e57f58dbbc7704f3c4738807c096c6833fd7fd
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-09 23:18:29 +0900

    メソッドチェーンを自作

---
+++ src/3-4_methodChain.ts
@@ -0,0 +1,27 @@
+/**
+ * メソッドチェーンをクロージャで実装
+ * @url https://mnivore.com/blogs/archives/49
+ */
+const SimpleCalc = () => {
+  let val = 0;
+  return {
+    add: function(num: number) {
+      val += num;
+      // 値ではなくこの関数自身への参照を返す
+      return this;
+    },
+    val: () => val,
+  };
+};
+
+const simpleCalc = SimpleCalc();
+simpleCalc
+  .add(3)
+  .add(4)
+  .add(5)
+  ;
+
+const result = simpleCalc
+  .val(); // 遅延実行
+  
+console.log(result); // -> 12
