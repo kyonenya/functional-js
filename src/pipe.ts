fbfc5e048e8eba32882d481d03c73949a1b07e55
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-11 08:37:42 +0900

    pipe関数にジェネリクスで型付け

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,16 +1,21 @@
 {
 let l = 'End';
-const pipe = (x: any) => {
-  return (fn: Function) => {
-//    if (!fn) return x;
+
+type Pipe<T> = {
+  (): T;
+  <U>(fn: (x: T) => U): Pipe<U>;
+}
+
+const pipe = <T>(x: T) => {
+  return <U>(fn: (x: T) => U) => {
     return pipe(fn(x));
   };
 };
 
 pipe(100)
-  ((x: number) => x * 1.1)
-  ((x: number) => x.toString())
-  ((x: string) => console.log(x));
+  (x => x * 1.1)
+  (x => x.toString())
+  (x => console.log(x));
 //  ();
 
 console.log(l);
