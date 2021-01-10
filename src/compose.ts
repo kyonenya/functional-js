ae90a18e25b093f168575ed7eae3834d515b7872
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-11 08:06:28 +0900

    汎用的なcompose関数を自作

--- src/compose.ts
+++ src/compose.ts
@@ -1,5 +1,7 @@
+{
 let l;
 
+const trim = (str: string) => str.replace(' ', '');
 const toInt = (numStr: string) => parseInt(numStr);
 const taxize = (price: number) => price * 1.1;
 taxize(toInt('100')); // 110
@@ -10,6 +12,16 @@ const compose2 = (fn1: Function, fn2: Function) => {
 
 // from right to left
 const strToTax = compose2(taxize, toInt);
-strToTax(100).log();
+strToTax(100);
+
+/** compose any number of functions */
+const compose = (..._fns: Function[]) => {
+  const fns = [..._fns];
+  return (x: unknown) => fns.reverse().reduce((acc, fn) => fn(acc), x);
+};
+
+const adjustStrToTax = compose(taxize, toInt, trim);
+l = adjustStrToTax('  100 '); // 110
 
 console.log(l);
+}
