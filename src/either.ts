5fc901620edae92a58b96e5e88b00bbb2b37e127
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-14 08:02:23 +0900

    EitherインターフェースをRight・Leftに実装させた

--- src/either.ts
+++ src/either.ts
@@ -3,23 +3,27 @@ import * as R from 'ramda';
 let l;
 
 type f<T, U> =(x: T) => U;
-type Either<T> = Left<T>|Right<T>;
+//type Either<T> = Left<T>|Right<T>;
 
-const ofEither = <T>(a: T) => (a !== null && a !== undefined)
-  ? new Right(a)
-  : new Left(a);
 const ofRight = <T>(val: T) => new Right(val);
 const ofLeft = <T>(val: T) => new Left(val);
 
-class Left<T> {
+interface Either<T> {
+  map: <U>(fn: f<T, U>) => Left<T>|Right<U>;
+  getOrElse: <U>(other: U) => U|T;
+  orElse: <U>(fn: f<T, U>) => U|Right<T>;
+  value: void|T;
+}
+
+class Left<T> implements Either<T> {
   constructor(private _value: T) {}
-  public map = () => this; // => Left (skipped)
+  public map = (_: Function) => this; // => Left (skipped)
   get value() { throw new TypeError('Can not extract the value of Left') }
   public getOrElse = <U>(other: U) => other;
   public orElse = <U>(fn: f<T, U>)=> fn(this._value); // => U
 }
 
-class Right<T> {
+class Right<T> implements Either<T> {
   constructor(private _value: T) {}
   public map = <U>(fn: f<T, U>) => ofRight(fn(this._value)); // => Right<U>
   get value() { return this._value };
@@ -27,22 +31,28 @@ class Right<T> {
   public orElse = (_: Function) => this; // => Right (skipped)
 }
 
-//
+/** 
+ * Maybe-like Either (Safe execution)
+ */
+const ofEither = <T>(a: T) => (a !== null && a !== undefined)
+  ? new Right(a)
+  : new Left(a);
+
 const idsDb = { 'Alex': 1, 'Bob': 2, 'Catherine': 3 };
 const selectById = (ids: { [k: string]: number }) => {
-  return (name: string) => ofEither(R.prop(name, ids));
+  return (name: string) => ofEither(R.prop(name, ids)); // from nullable
 };
-const findId: (name: string) => Either<number> = selectById(idsDb);
+const safeFindId: (name: string) => Either<number> = selectById(idsDb);
 
-findId('Catherine') // = Right(3)
+safeFindId('Catherine') // = Right(3)
   .orElse(R.tap(console.log)) // (skipped)
   .map(R.tap(console.log)); // -> 3
-
-//findId('Takashi')
+//safeFindId('Takashi')
 //  .value; // -> Uncaught TypeError: Can not extract the value of Left
-
-findId('Takashi')
+safeFindId('Takashi')
   .orElse(R.tap(console.log)); // -> undefined
+
+
 
 console.log(l);
 }
