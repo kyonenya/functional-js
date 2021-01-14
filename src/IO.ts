f9fed74eeccf430a76f7bde461df0b0bb4392477
Takashi Kyonenya <kyonenya.takashi@gmail.com>
2021-01-14 14:41:57 +0900

    IOモナドを実装してfsを遅延実行。副作用関数をカプセル化して、パイプラインもそこに繋げて、あとは実行を待つだけの一連の副作用関数を作る

--- src/IO.ts
+++ src/IO.ts
@@ -1,10 +1,28 @@
 import fs from 'fs';
 import path from 'path';
 {
-const path1 = path.resolve(__dirname, 'assets', 'data1.txt'); // __dirname = rootDir
-fs.readFile(path1, 'utf-8', (err, data) => {
-  if (err) console.error(err);
-  console.log(data);
-});
+let l: any = 'End';
 
+class IO {
+  constructor(private effect: Function) {};
+  public run = () => this.effect();
+  public map = (fn: Function) => {
+    const self = this;
+    return new IO(() => fn(self.effect())); // compose
+  }
+}
+const toIO = (fn: Function) => new IO(fn); // Lift funcion to monad
+
+/** fs (not async) */
+const getTxt = (path: string) => () => {
+  const data = fs.readFileSync(path, 'utf-8');
+  return data;
+}
+const txtToUpperCase = toIO(getTxt(path.resolve(__dirname, 'assets', 'data1.txt')))
+  .map((x: string) => x.toUpperCase());
+
+const result = txtToUpperCase.run();
+l = result;
+
+console.log(l);
 }
